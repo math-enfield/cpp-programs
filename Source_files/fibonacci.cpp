c76782ccb1294b22ebc888257c5860cd430c47d3
Byteknight <byteknight007@gmail.com>
2026-03-23 01:13:26 +0530

    initial draft- wrong code, logic error

---
+++ Source_files/fibonacci.cpp
@@ -0,0 +1,17 @@
+//this is the code for printing a certain number of fibonacci sequence as input by the user
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=0,b=1;
+    int c, num;
+    cout << "Enter the number of Fibonacci sequence you want to print: ";
+    cin >> num;
+    for (int i=0;i<=num;i++){
+        c=b+a;
+        cout << a << b << c;
+        a=b;
+        b=c;
+    }
+    return 0;
+}
