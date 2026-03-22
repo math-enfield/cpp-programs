08c440f8bb35394c9fb4eff01c243c95419215f5
Byteknight <byteknight007@gmail.com>
2026-03-22 19:27:55 +0530

    program for printing multiplication table of a number input by user

---
+++ Source_files/multiplication_table.cpp
@@ -0,0 +1,19 @@
+//this is the simple code for printing a multiplication table of a number input by the user
+#include <iostream>
+using namespace std;
+
+void Table(int z){
+    int multiple;
+    for (int i=0; i=10; i++) {
+        multiple = i*z;
+        cout << z << 'x' << i << '=' << multiple << endl;
+    }
+}
+
+int main(){
+    int num;
+    cout << "Enter the number you want the multiplication table of: ";
+    cin >> num;
+    Table(num);
+    return 0;
+}
