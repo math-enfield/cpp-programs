58d26c85d4ced6b18194a6bd69ceb175891a689b
Byteknight <byteknight007@gmail.com>
2026-03-23 01:40:24 +0530

    fix the logic, add if-else-if statements

--- Source_files/fibonacci.cpp
+++ Source_files/fibonacci.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 int main(){
-    int a=0,b=1;
-    int c, num;
-    cout << "Enter the number of Fibonacci sequence you want to print: ";
+    int a=0,b=1,c,num;
+    cout << "Enter the number of Fibonacci terms you want to print: ";
     cin >> num;
-    cout<<"01";
-    for (int i=0;i<num-2;i++){
-        c=b+a;
-        cout << c;
-        a=b;
-        b=c;
+    for (int i=0;i<num;i++){
+        if (i==0) cout << a;
+        else if (i==1) cout << " "<< b;
+        else{
+            c=b+a;
+            a=b;
+            b=c;
+            cout << " " << b;
+        }
     }
     return 0;
 }
